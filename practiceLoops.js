bcb328b331d4e593e451cd0b2e98f50ee58b4251
Sherma'n Wilson <84743550+shermanawilson@users.noreply.github.com>
2021-06-07 02:53:08 -0500

    Add files via upload

---
+++ practiceLoops.js
@@ -0,0 +1,9 @@
+// alert("Hey");
+function showMultiplicationTable(number) {
+	for(var i = 1; i <= 10; i++) {
+
+		console.log(number + " x " + i + " = " + (i*number));
+	}
+
+}
+alert(showMultiplicationTable(7));
