01f1e1f02cbce0eeb9de0360651dd8104357deb3
Sherma'n Wilson <84743550+shermanawilson@users.noreply.github.com>
2021-07-03 15:07:18 -0500

    Delete practiceLoops.js

--- practiceLoops.js
+++
@@ -1,9 +0,0 @@
-// alert("Hey");
-function showMultiplicationTable(number) {
-	for(var i = 1; i <= 10; i++) {
-
-		console.log(number + " x " + i + " = " + (i*number));
-	}
-
-}
-alert(showMultiplicationTable(7));
